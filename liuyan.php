4636855793aa6dd706184e1c5ee0c55f2e86f82a
柯冠州 <kk2247@users.noreply.github.com>
2017-03-08 10:27:55 +0800

    Update liuyan.php

--- liuyan.php
+++ liuyan.php
@@ -1,6 +1,9 @@
-
+<?php 
+mysql_query("set name 'utf8'");
+?>
 <html xmlns="http://www.w3.org/1999/xhtml">
 <meta http-equiv="Content-Type" content="text/html; charset=utf-8" />
+  <head>
 <title>liuyan</title>
 </head>
 <body>
@@ -9,7 +12,7 @@
 <p>word</p>
 <p>&nbsp;</p>
 <form name="form2" method="post" action="test3.php">
-<p align="center"><input type="text" name="content" id="content" cols="45" rows="5" maxlength="200" ></textarea>
+<p align="center"><input type="text" name="content" id="content" cols="45" rows="5" maxlength="200" >
 </p>
 </table>
 <p>
